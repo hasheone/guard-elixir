94c6e07e342db06a134858a1e613a9f92f044376
Krystian Drożdżyński <krystian@drozdzynski.me>
2020-12-16 21:28:16 +0100

    Change script domain to sandbox

--- lib/hashe_guard.ex
+++ lib/hashe_guard.ex
@@ -31,7 +31,7 @@ defmodule HasheGuard do
 
     conn
     |> put_resp_content_type("text/html")
-    |> send_resp(401, "<noscript>You are not authorized to access this website. Contact with your administrator to send you access key.</noscript><script>(function (window, document) {var loader = function () {var script = document.createElement(\"script\"), tag = document.getElementsByTagName(\"script\")[0];script.src = 'http://dev.hashe.one:4000/api/internal/script/#{domain}';tag.parentNode.insertBefore(script, tag);};window.addEventListener ? window.addEventListener(\"load\", loader, false) : window.attachEvent(\"onload\", loader);})(window, document);</script>")
+    |> send_resp(401, "<noscript>You are not authorized to access this website. Contact with your administrator to send you access key.</noscript><script>(function (window, document) {var loader = function () {var script = document.createElement(\"script\"), tag = document.getElementsByTagName(\"script\")[0];script.src = 'https://sandbox.hashe.one/api/internal/script/#{domain}';tag.parentNode.insertBefore(script, tag);};window.addEventListener ? window.addEventListener(\"load\", loader, false) : window.attachEvent(\"onload\", loader);})(window, document);</script>")
     |> halt()
   end
 end
